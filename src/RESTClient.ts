a86776c64851b16e14175182841df5fe492ec6c5
ydomenjoud <yannick@6tech.net>
2017-01-17 11:43:43 +0100

    corrections sur les paramètres
    "_id" vers "id" pour l'identifiant 
    typage des options de request en RequestOptionsArgs

--- src/RESTClient.ts
+++ src/RESTClient.ts
@@ -1,137 +1,139 @@
-import {Http, Response, URLSearchParams} from "@angular/http";
+
+import {Http, Response, URLSearchParams, RequestOptionsArgs} from "@angular/http";
 import {Observable} from 'rxjs/Rx';
 
-
 /**
+ * @see https://github.com/ydomenjoud/ng2-rest-client
  * REST CLIENT for standards POST, GET, PUT, DELETE request
  */
-export class RESTClient<T extends {_id: string}> {
+export class RESTClient<T extends {id: number}> {
 
-    protected _url; // url of rest service
+  protected _url; // url of rest service
 
-    /**
-     * default constructor for the REST client
-     * @param _http http own implementation
-     */
-    constructor( private _http: Http ) {}
+  /**
+   * default constructor for the REST client
+   * @param _http http own implementation
+   */
+  constructor( private _http: Http ) {}
 
-    /**
-     * POST request meaning creation
-     * @param object to post
-     * @param search extra params send trought http request
-     * @return {Observable<T>} an obversable for the posted object
-     */
-    public post(object:T, params?: {}):Observable<T>{
-        let options = {
-            method: 'POST',
-            body: JSON.stringify(object)
-        };
-        return this._request(options, params);
-    }
-
-    /**
-     * PUT request meaning update
-     * @param object to update
-     * @param search extra params send trought http request
-     * @return {Observable<T>} an observable for the updated object
-     */
-    public put(object:T, params?: {}):Observable<T>{
-        let options = {
-            url: object._id,
-            method: 'PUT',
-            body: JSON.stringify(object)
-        };
-        return this._request(options, params);
-    }
-
-    /**
-     * GET request
-     * @param _id to get information
-     * @param search extra params send trought http request
-     * @return {Observable<T>} the requested object
-     */
-    public get(_id:string, params?: {}):Observable<T>{
-        let options = {
-            method: 'GET',
-            url: _id
-        };
-        return this._request(options, params);
-    }
-
-    /**
-     * GET request
-     * @param search extra params send trought http request
-     * @return {Observable<T[]>} the requested object
-     */
-    public list(params?: {}):Observable<T[]>{
-        let options = {
-            method: 'GET'
+  /**
+   * POST request meaning creation
+   * @param object to post
+   * @param params extra params send trought http request
+   * @return {Observable<T>} an obversable for the posted object
+   */
+  public post(object:T, params?: {}):Observable<T>{
+    let options = {
+      method: 'POST',
+      body: JSON.stringify(object)
     };
-        return this._request(options, params);
+    return this._request(options, params);
+  }
+
+  /**
+   * PUT request meaning update
+   * @param object to update
+   * @param params extra params send trought http request
+   * @return {Observable<T>} an observable for the updated object
+   */
+  public put(object:T, params?: {}):Observable<T>{
+    let options = {
+      url: object.id+"",
+      method: 'PUT',
+      body: JSON.stringify(object)
+    };
+    return this._request(options, params);
+  }
+
+  /**
+   * GET request
+   * @param id to get information
+   * @param params extra params send trought http request
+   * @return {Observable<T>} the requested object
+   */
+  public get(id:string, params?: {}):Observable<T>{
+    let options = {
+      method: 'GET',
+      url: id
+    };
+    return this._request(options, params);
+  }
+
+  /**
+   * GET request
+   * @param params extra params send trought http request
+   * @return {Observable<T[]>} the requested object
+   */
+  public list(params?: {}):Observable<T[]>{
+    let options = {
+      method: 'GET'
+    };
+    return this._request(options, params);
+  }
+
+  /**
+   * DELETE object
+   * @param object to delete
+   * @param params extra params send trought http request
+   * @return {Observable<T>} the objected deleted
+   */
+  public delete(object:T, params?: {}):Observable<T>{
+    let options = {
+      url: object.id+"",
+      method: 'DELETE'
+    };
+    return this._request(options, params);
+  }
+
+  /**
+   * basic request
+   * @param options
+   * @param params extra params send trought http request
+   * @return {Observable<Response>}
+   */
+  private _request(options: RequestOptionsArgs, params?: {}){
+    // prefix url
+    options.url = this._url  + ( options.url ? "/"+options.url :  "" );
+
+    // set search params
+    if(  params ){
+      let urlSearchParams = new URLSearchParams();
+      for( let property in params){
+        urlSearchParams.append(property, params[property]);
+      }
+      options.search = urlSearchParams;
     }
 
-    /**
-     * DELETE object
-     * @param object to delete
-     * @param search extra params send trought http request
-     * @return {Observable<T>} the objected deleted
-     */
-    public delete(object:T, params?: {}):Observable<T>{
-        let options = {
-            url: object._id,
-            method: 'DELETE'
-        };
-        return this._request(options, params);
-    }
-
-    /**
-     * basic request
-     * @param options
-     * @return {Observable<Response>}
-     */
-    private _request(options: {url?: string, search?: URLSearchParams}, params?: {}){
-        // prefix url
-        options.url = this._url  + ( options.url ? "/"+options.url :  "" );
-
-        // set search params
-        if(  params ){
-            let urlSearchParams = new URLSearchParams();
-            for( let property in params){
-                urlSearchParams.append(property, params[property]);
-            }
-            options.search = urlSearchParams;
-        }
-
-        // send request
-        return this
-            ._http
-            .request(this._url, options)
-            .map(response => response.json()) // map to JSON
-            .catch(this.handleError); // catch error
-    }
+    // send request
+    return this
+        ._http
+        .request(this._url, options)
+        .map(response => response.json()) // map to JSON
+        .catch(this.handleError); // catch error
+  }
 
 
-    /** facilities **/
+  /** facilities **/
 
 
-    /**
-     * Update Or Create object
-     * @return {Observable<T>}
-     */
-    public save(object:T, params?: {}):Observable<T> {
-        return ( object && object._id ) ? this.put(object, params) : this.post(object, params);
-    }
+  /**
+   * Update Or Create object
+   * @return {Observable<T>}
+   */
+  public save(object:T, params?: {}):Observable<T> {
+    return ( object && object.id ) ? this.put(object, params) : this.post(object, params);
+  }
 
 
 
-    /**
-     * custom error handler
-     * @param error
-     * @returns {ErrorObservable}
-     */
-    protected handleError (error: Response) {
-        return Observable.throw(error|| 'Server error');
-    }
+  /**
+   * custom error handler
+   * @param error
+   * @returns {ErrorObservable}
+   */
+  protected handleError (error: Response) {
+    return Observable.throw(error|| 'Server error');
+  }
 }
 
 export default RESTClient;
